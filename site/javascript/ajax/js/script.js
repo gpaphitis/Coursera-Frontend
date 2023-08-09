d79b344b178de4a682ffb42f0a763a485eb5a423
gpaphitis <giorgospaphitis@gmail.com>
2023-08-09 15:13:50 +0300

    Changed url to be relative

--- site/javascript/ajax/js/script.js
+++ site/javascript/ajax/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) =>{
     document.querySelector('button').addEventListener('click',(event) => {
         // Semds GET request using the ajaxUtils object we exposed
         // The url is the one live server uses
-        $ajaxUtils.sendGetRequest('http://127.0.0.1:5500/javascript/ajax/data/names.txt', (request) => {
+        $ajaxUtils.sendGetRequest('data/names.txt', (request) => {
             // request.responseText holds the response from the server
             let name=request.responseText;
             // This must be placed here so it will be porcessed when the request is done since it is used as our responseHandler method
